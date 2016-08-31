be5be185d2dad6f3aa65922913ae4bc0c0fe294d
Jakub Pavlik <pavlk.jakub@gmail.com>
2016-08-31 16:32:09 +0200

    Gerrit theme css

--- gerrit-entrypoint.sh
+++ gerrit-entrypoint.sh
@@ -76,6 +76,10 @@ if [ "$1" = "/gerrit-start.sh" ]; then
     [ -z "${DB_ENV_MYSQL_PASSWORD}" ] || set_secure_config database.password "${DB_ENV_MYSQL_PASSWORD}"
   fi
 
+  #Section theme
+  set_gerrit_config gerrit.canLoadInIframe "true"
+  mv /tmp/GerritSite.css ${GERRIT_SITE}/etc/
+
   #Section auth
   [ -z "${AUTH_TYPE}" ]           || set_gerrit_config auth.type "${AUTH_TYPE}"
   [ -z "${AUTH_HTTP_HEADER}" ]    || set_gerrit_config auth.httpHeader "${AUTH_HTTP_HEADER}"
